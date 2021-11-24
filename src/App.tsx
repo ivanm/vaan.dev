f9d9806d43c500650a8fdffef45f562d04fb7eea
Ivan Mayoral <iimayoral@gmail.com>
2021-11-23 19:19:18 -0600

    Adding Button for language

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import * as React from "react";
-import { ChakraProvider, Box, Grid, Flex } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  Box,
+  Button,
+  Grid,
+  Flex,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import Intro from "./Intro";
 import theme from "./theme";
@@ -8,7 +19,22 @@ export const App = () => (
   <ChakraProvider theme={theme}>
     <Box fontSize="md">
       <Grid minH="100vh" pr={10} pl={10} templateRows="80px 1fr">
-        <ColorModeSwitcher mt={2}justifySelf="flex-end" />
+        <Flex mt={5} direction="row" justifyContent="end">
+          <Menu mr={3}>
+            <MenuButton
+              variant="ghost"
+              as={Button}
+              rightIcon={<ChevronDownIcon />}
+            >
+              English
+            </MenuButton>
+            <MenuList minWidth="115px">
+              <MenuItem>English</MenuItem>
+              <MenuItem onClick={() => alert("Kagebunshin")}>Spanish</MenuItem>
+            </MenuList>
+          </Menu>
+          <ColorModeSwitcher justifySelf="flex-end" />
+        </Flex>
         <Flex justify="center" mt={10} width="100%">
           <Intro />
         </Flex>
